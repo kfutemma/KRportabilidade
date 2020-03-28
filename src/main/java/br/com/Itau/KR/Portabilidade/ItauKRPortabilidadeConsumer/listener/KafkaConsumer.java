d5432c421e86d52dba97c47a223c8db8eb0f4561
Kaique Futemma <kaiquefutemma@gmail.com>
2020-03-28 17:21:47 -0300

    first commit

---
+++ src/main/java/br/com/Itau/KR/Portabilidade/ItauKRPortabilidadeConsumer/listener/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package br.com.Itau.KR.Portabilidade.ItauKRPortabilidadeConsumer.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+	
+	@KafkaListener(topics = "PrimeiroTopico", groupId = "KR_Portabilidade")
+	public void consume(String message) {
+		System.out.println("Consumed message: " + message);
+	}
+
+}
---
+++ src/test/java/br/com/Itau/KR/Portabilidade/ItauKRPortabilidadeConsumer/ItauKrPortabilidadeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Itau.KR.Portabilidade.ItauKRPortabilidadeConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauKrPortabilidadeConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
